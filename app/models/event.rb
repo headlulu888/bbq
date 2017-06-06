2556c65e15520d5bf47457664d701c753558a5ce
Андрей Махнев <mahnev@MacBook-Air-Andrej.local>
2017-06-06 09:59:23 +0300

    bootstrap installed

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :user, presence: true 
+  validates :user, presence: true
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
